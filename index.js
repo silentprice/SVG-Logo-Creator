43ae176d5b0496a38da78461909fdb33fbf5009d
Nick <nprice0502@yahoo.com>
2023-06-07 15:01:55 -0400

    created generateSVG function and installed jest

--- index.js
+++ index.js
@@ -30,3 +30,28 @@ async function promptUser() {
 
   return response;
 }
+
+function generateSVG(response) {
+    const { text, textColor, shape, shapeColor } = response;
+  
+    const svgContent = `
+      <svg width="300" height="200">
+        <rect width="300" height="200" fill="${shapeColor}" />
+        <text x="150" y="100" fill="${textColor}" text-anchor="middle">${text}</text>
+      </svg>
+    `;
+      fs.writeFile('logo.svg', svgContent, (err) => {
+      if (err) {
+        console.error('Error creating SVG file:', err);
+      } else {
+        console.log('Generated logo.svg');
+      }
+    });
+  }
+  async function run() {
+    const response = await promptUser();
+    generateSVG(response);
+  }
+  
+  run();
+  
